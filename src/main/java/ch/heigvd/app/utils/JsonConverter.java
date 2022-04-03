f33ced73801bdc6e61f29cf63c096b094c46cf15
guilain <gmbayo11@gmail.com>
2022-04-03 11:11:32 +0200

    create class JsonConverter

---
+++ src/main/java/ch/heigvd/app/utils/JsonConverter.java
@@ -0,0 +1,14 @@
+package ch.heigvd.app.utils;
+
+/**
+ * JsonConverter class
+ */
+public class JsonConverter {
+
+}
+
+class JsonConfig {
+    String title;
+    String lang;
+    String charset;
+}
